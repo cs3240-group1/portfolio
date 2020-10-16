278f7e6c7723f9e368c573b4a8d107afc783325b
tanamaroby <tanamaroby@gmail.com>
2020-10-16 09:36:32 +0800

    Updates

---
+++ precache-manifest.550c5f9972ff5eecd6ecc8dc2c575333.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1760e4b3feec69fac7ae7b0324623ee",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "498594cdde4225b39d46",
+    "url": "/portfolio/static/css/main.b101dffd.chunk.css"
+  },
+  {
+    "revision": "f2421cca5c5b1dcf7696",
+    "url": "/portfolio/static/js/2.d4d4f36c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/portfolio/static/js/2.d4d4f36c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "498594cdde4225b39d46",
+    "url": "/portfolio/static/js/main.d8520ddc.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "c95d24a04c604eaf6fb73267df266ae6",
+    "url": "/portfolio/static/media/Group1.c95d24a0.png"
+  },
+  {
+    "revision": "7775c7e8c07a85591b64078aceee6762",
+    "url": "/portfolio/static/media/idea.7775c7e8.png"
+  },
+  {
+    "revision": "89b69e45b251daed7c1a5cb3826ac84d",
+    "url": "/portfolio/static/media/logo.89b69e45.png"
+  }
+]);
